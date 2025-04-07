ba126d3c875c4484f9aa5d2f33ea9c407539ec0f
Harsh Jain <harsh.jain@energyexemplar.com>
2025-04-07 19:00:22 +0530

    set default value of password

--- TravelMemories/Contracts/Data/configs/UserInfoConfig.cs
+++ TravelMemories/Contracts/Data/configs/UserInfoConfig.cs
@@ -13,6 +13,7 @@ namespace TravelMemories.Contracts.Data.configs
             builder.Property(x => x.Name).IsRequired();
             builder.Property(x => x.Email).IsRequired();
             builder.Property(x => x.IsManualLogin).HasDefaultValue(false);
+            builder.Property(x => x.Password).HasDefaultValue("");
             
             // this will make email address unique such that no users will have the same address and it will make lookups faster
             builder.HasIndex(x => x.Email).IsUnique();
