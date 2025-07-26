b92afce27120128d8222f41bf3fba52094f7f5c4
Harsh Jain <harsh.jain@energyexemplar.com>
2025-07-26 19:59:44 +0530

    add domain in cookie settings

--- TravelMemories/Controllers/Authentication/LoginController.cs
+++ TravelMemories/Controllers/Authentication/LoginController.cs
@@ -165,6 +165,7 @@ namespace TravelMemories.Controllers.Authentication
                 Secure = true,
                 IsEssential = true,
                 SameSite = SameSiteMode.None,
+                Domain = ".harshjain17.com",
             });
         }
 
