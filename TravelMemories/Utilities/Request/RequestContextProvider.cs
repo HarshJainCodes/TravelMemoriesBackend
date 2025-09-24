752c8a5c531e291072a2e831a16c0e8b671036d7
Harsh Jain <harshjain17may@gmail.com>
2025-09-24 09:43:37 +0530

    testing mcp changes

--- TravelMemories/Utilities/Request/RequestContextProvider.cs
+++ TravelMemories/Utilities/Request/RequestContextProvider.cs
@@ -14,14 +14,21 @@ namespace TravelMemories.Utilities.Request
         public JwtSecurityToken GetJWTToken()
         {
             string JWTTokenFromCookie = _httpContextAccessor.HttpContext.Request.Cookies["travelMemoriestoken"];
+            string JWTFromAuthHeader = null;
 
-            if (JWTTokenFromCookie == null)
+            var JwtAuthHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(JwtAuthHeader))
+            {
+                JWTFromAuthHeader = JwtAuthHeader.ToString().Substring("Bearer ".Length);
+            }
+
+            if (JWTTokenFromCookie == null && JWTFromAuthHeader == null)
             {
                 throw new ArgumentException("This is not a valid request");
             }
 
             var tokenHandler = new JwtSecurityTokenHandler();
-            var jwtToken = tokenHandler.ReadJwtToken(JWTTokenFromCookie);
+            var jwtToken = tokenHandler.ReadJwtToken(JWTFromAuthHeader ?? JWTTokenFromCookie);
 
             return jwtToken;
         }
---
+++ TravelMemoriesBackend.ApiClient/TripData/TripDataClient.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using TravelMemoriesBackend.Contracts.Storage;
+
+namespace TravelMemoriesBackend.ApiClient.TripData
+{
+    public class TripDataClient : ITripDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions jsonSerializerOptions;
+
+        public TripDataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+        }
+
+        public async Task<int> GetTotalTripsCountAsync()
+        {
+            return 30;
+        }
+
+        public async Task<List<ImageData>> GetAllTripDataAync(string token)
+        {
+            string url = $"/ImageUpload/AllTripData";
+
+            var res = await SendRequestAsync<List<ImageData>>(HttpMethod.Get, url, token);
+            return res;
+        }
+
+        public async Task<TResponse> SendRequestAsync<TResponse>(HttpMethod httpMethod, string url, string accessToken)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(httpMethod, url);
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException($"Failed to fetch all trips data, {response.ReasonPhrase}, status code: {response.StatusCode}");
+            }
+
+            var data = JsonSerializer.Deserialize<TResponse>(await response.Content.ReadAsStringAsync(), jsonSerializerOptions);
+            return data;
+
+        }
+    }
+}
