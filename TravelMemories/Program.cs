dc3f0ed4eba0e08e628542a99e15804546023d1e
Harsh Jain <harshjain17may@gmail.com>
2025-09-29 15:52:47 +0530

    test2

--- TravelMemories/Program.cs
+++ TravelMemories/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddScoped<IImageCompressService, ImageCompressService>();
 builder.Services.AddScoped<IRequestContextProvider, RequestContextProvider>();
 builder.Services.AddScoped<LoginController, LoginController>();
 
+
 builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
 {
     ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"],
