d92523edd49fff9867b33148397395e827d46f71
Harsh Jain <harshjain17may@gmail.com>
2025-10-16 17:14:43 +0530

    testing if new project builds in azure

---
+++ TravelMemoriesBackend.Contracts/Storage/ImageData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelMemoriesBackend.Contracts.Storage
+{
+    public class ImageData
+    {
+        public string TripTitle { get; set; }
+        public string Email { get; set; }
+
+        public float Lat { get; set; }
+
+        public float Lon { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> ImageUrls { get; set; } = new List<string>();
+    }
+}
