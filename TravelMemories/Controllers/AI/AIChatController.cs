e35abd9b27da52df8c6e5e843ac3133c831b3777
Harsh Jain <harshjain17may@gmail.com>
2025-11-21 23:36:07 +0530

    add chat history feature

--- TravelMemories/Controllers/AI/AIChatController.cs
+++ TravelMemories/Controllers/AI/AIChatController.cs
@@ -1,36 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Protocol;
 using ModelContextProtocol.SemanticKernel.Extensions;
 using Newtonsoft.Json.Linq;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
+using TravelMemories.Database;
 using TravelMemories.Utilities.Request;
+using TravelMemoriesBackend.Contracts.Data;
 
 namespace TravelMemories.Controllers.AI
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class AIChatController : ControllerBase
     {
 
         private readonly IRequestContextProvider _requestContextProvider;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
+        private readonly ImageMetadataDBContext _imageMetadataDBContext;
 
 
-        public AIChatController(IRequestContextProvider requestContextProvider, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        public AIChatController(IRequestContextProvider requestContextProvider, 
+            IHttpClientFactory httpClientFactory, 
+            IConfiguration configuration,
+            ImageMetadataDBContext imageMetadataDBContext)
         {
             _requestContextProvider = requestContextProvider;
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
+            _imageMetadataDBContext = imageMetadataDBContext;
+        }
+
+        [HttpGet]
+        [Route("GetMessages")]
+        public async Task<ActionResult<List<MessageFromConversationDTO>>> GetConversationMessages([FromQuery] Guid conversationId)
+        {
+            var token = _requestContextProvider.GetJWTToken();
+            string userEmail = token.Claims.Where(c => c.Type == "email").First().Value;
+
+            // user should be the owner of the conversation
+            ChatConversation conv = _imageMetadataDBContext.ChatbotConversations.Where(conversation => conversation.ConversationId == conversationId).FirstOrDefault();
+            if (conv == null)
+            {
+                return BadRequest("please provide a valid conversation id");
+            }
+            if (conv.UserEmail != userEmail)
+            {
+                return Unauthorized("You do not have access to this conversation");
+            }
+
+            var messages = await _imageMetadataDBContext.ChatMessages.Where(message => message.ConversationId == conversationId).Select(message => new MessageFromConversationDTO
+            {
+                Message = message.Message,
+                MessageId = message.MessageId,
+                CreatedAt = message.CreatedAt,
+                Role = message.MessageRole,
+            }).ToListAsync();
+
+            return Ok(messages);
+        }
+
+        [HttpGet]
+        [Route("Conversations")]
+        public async Task<List<SideConversationsResponseDTO>> GetSideConversations()
+        {
+            var token = _requestContextProvider.GetJWTToken();
+            string userEmail = token.Claims.Where(c => c.Type == "email").First().Value;
+
+            return await _imageMetadataDBContext.ChatbotConversations.Where(conversation => conversation.UserEmail == userEmail).Select(conversation => new SideConversationsResponseDTO
+            {
+                ConversationId = conversation.ConversationId,
+                ConversationName = conversation.ConversationName,
+                CreatedAt = conversation.CreatedAt,
+            }).ToListAsync();
         }
 
         [HttpPost]
-        public async Task<string> GetAIResponse(UserPrmopt userPrompt)
+        public async Task<ChatbotResponse> GetAIResponse(UserPrmopt userPrompt)
         {
             JwtSecurityToken jwtToken = _requestContextProvider.GetJWTToken();
             string userEmail = jwtToken.Claims.Where(c => c.Type == "email").First().Value;
@@ -56,11 +111,88 @@ namespace TravelMemories.Controllers.AI
                 FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
             };
 
-            var history = new ChatHistory();
-            history.AddUserMessage(userPrompt.Prompt);
+            var chatHistory = new ChatHistory();
 
-            var result = await chatCompletionService.GetChatMessageContentAsync(history, executionSettings: openAIPromptExecutionSettings, kernel: kernel);
-            return result.Content;
+            Guid responseConversationId;
+            if (userPrompt.ConversationId == null)
+            {
+                // create a new conversation
+                Guid currentConvId = Guid.NewGuid();
+                responseConversationId = currentConvId;
+                ChatConversation currentConv = new ChatConversation
+                {
+                    ConversationId = currentConvId,
+                    UserEmail = userEmail,
+                    ConversationName = "Default Name",
+                    CreatedAt = DateTime.UtcNow,
+                };
+
+                _imageMetadataDBContext.ChatMessages.Add(new ChatMessage
+                {
+                    MessageId = Guid.NewGuid(),
+                    ConversationId = currentConvId,
+                    Message = userPrompt.Prompt,
+                    MessageRole = ModelContextProtocol.Protocol.Role.User,
+                    CreatedAt = DateTime.UtcNow,
+                });
+
+                chatHistory.AddUserMessage(userPrompt.Prompt);
+
+                // get a name for this conversation
+                var tempChatHistory = new ChatHistory();
+                tempChatHistory.AddUserMessage($"This is the prompt the user has provided: ${userPrompt.Prompt}. Suggest a conversation name based on this prompt. Only give the name as output nothing else. Do not include quotes");
+                var newConvName = await chatCompletionService.GetChatMessageContentAsync(tempChatHistory, executionSettings: openAIPromptExecutionSettings, kernel: kernel);
+                currentConv.ConversationName = newConvName.Content;
+                _imageMetadataDBContext.ChatbotConversations.Add(currentConv);
+
+            }
+            else
+            {
+                responseConversationId = userPrompt.ConversationId.GetValueOrDefault();
+
+                _imageMetadataDBContext.ChatMessages.Add(new ChatMessage
+                {
+                    MessageId = Guid.NewGuid(),
+                    ConversationId = responseConversationId,
+                    Message = userPrompt.Prompt,
+                    MessageRole = ModelContextProtocol.Protocol.Role.User,
+                    CreatedAt = DateTime.UtcNow,
+                });
+
+                await _imageMetadataDBContext.SaveChangesAsync();
+
+                // this chat is already a part of already existing conversation
+                List<ChatMessage> messagesOfThisConversation = _imageMetadataDBContext.ChatMessages.Where(message => message.ConversationId == responseConversationId).OrderBy(message => message.CreatedAt).ToList();
+                messagesOfThisConversation.ForEach(message =>
+                {
+                    if (message.MessageRole == ModelContextProtocol.Protocol.Role.User)
+                    {
+                        chatHistory.AddUserMessage(message.Message);
+                    } else
+                    {
+                        chatHistory.AddAssistantMessage(message.Message);
+                    }
+                });
+            }
+
+            var result = await chatCompletionService.GetChatMessageContentAsync(chatHistory, executionSettings: openAIPromptExecutionSettings, kernel: kernel);
+
+            _imageMetadataDBContext.ChatMessages.Add(new ChatMessage
+            {
+                MessageId = Guid.NewGuid(),
+                ConversationId = responseConversationId,
+                Message = result.Content,
+                MessageRole = ModelContextProtocol.Protocol.Role.Assistant,
+                CreatedAt = DateTime.UtcNow,
+            });
+
+            await _imageMetadataDBContext.SaveChangesAsync();
+
+            return new ChatbotResponse
+            {
+                Content = result.Content,
+                ConversationId = responseConversationId
+            };
 
         }
 
@@ -80,6 +212,34 @@ namespace TravelMemories.Controllers.AI
 
     public class UserPrmopt
     {
+        public Guid? ConversationId { get; set; }
         public string Prompt { get; set; }
+    }
+
+    public class ChatbotResponse
+    {
+        public string Content { get; set; }
+
+        public Guid ConversationId { get; set; }
+    }
+
+    public class SideConversationsResponseDTO
+    {
+        public Guid ConversationId { get; set; }
+
+        public string ConversationName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class MessageFromConversationDTO
+    {
+        public Guid MessageId { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Role Role { get; set; }
     }
 }
